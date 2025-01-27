3c87f86cd598e50c6c1185d6e3417bc59a105c9d
Arvid Berlin <arvidberlinjobb@gmail.com>
2025-01-27 23:09:20 +0100

    Added specific CSS for buttons on start page and result page

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -3,7 +3,7 @@ import React from "react";
 // Show player result and option to play again
 const Result = ({ score, total, onRestart, onGoToStart }) => {
     return (
-        <div>
+        <div className="result-page">
             <h2>Quiz finished!</h2>
             <p>
                 You got {score} of {total} correct!
